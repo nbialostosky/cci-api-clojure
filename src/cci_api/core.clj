bf961f63bf2b6742078b0f3641bbb42adae69fe2
Nick Bialostosky <nick.bialostosky@circleci.com>
2020-09-07 22:22:46 -0500

    refactor

--- src/cci_api/core.clj
+++ src/cci_api/core.clj
@@ -2,32 +2,36 @@
 		(:gen-class)
 		(:require [clojure.data.json :as json]))
 
-;; Set workflow ID
-(def workflow "https://circleci.com/api/v2/workflow/a1a7e440-8861-4560-856c-6a43c85117a9")
+(def workflow "https://circleci.com/api/v2/workflow/")
 (def user "https://circleci.com/api/v2/user/")
 
 ;; Add personal API key here
 (def api-key "")
 
-(def workflow-url (str workflow "?circle-token=" api-key))
+;; Set workflow ID
+(def workflow-id "a1a7e440-8861-4560-856c-6a43c85117a9")
+
+(defn url 
+		[base-url base-id]
+		(str base-url base-id))
+
+(defn full-url
+		[url]
+		(str url "?circle-token=" api-key))
+
+(defn value
+		[response key]
+		(get response key))
 
 (def workflow-response (json/read-str
-  (slurp workflow-url)))
+  (slurp (full-url (url workflow workflow-id)))))
 
 (def user-id (get workflow-response "started_by"))
 
-(def user-url (str user user-id "?circle-token=" api-key))
-
 (def user-response (json/read-str
-  (slurp user-url)))
+  (slurp (full-url (url user user-id)))))
 
 ;; name can be nil
-(def name-value (get user-response "name"))
+(def user-name (cond (nil? (value user-response "name")) "No Name Provided" :else (value user-response "name")))
 
-(def name (cond (nil? name-value) "No Name Provided" :else name-value))
-
-(def login (get user-response "login"))
-
-(def id (get user-response "id"))
-
-(def who-dun-it? (str "The workflow was started by " name "/" login ", user-id for reference: " id))
+(def who-dun-it? (str "The workflow was started by " user-name "/" (value user-response "login") ", user-id for reference: " (value user-response "id")))
